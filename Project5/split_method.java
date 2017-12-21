dce7a5f8f2baf223ccc92e90c919e71d5b7fe14e
Nanako Chung <34701677+nanako-chung@users.noreply.github.com>
2017-12-20 23:50:09 -0500

    Create split_method.java

---
+++ Project5/split_method.java
@@ -0,0 +1,62 @@
+	/**
+	 * Splits the given line of a CSV file according to commas and double quotes
+	 * (double quotes are used to surround multi-word entries so that they may contain commas)
+	 * @author Joanna Klukowska
+	 * @param textLine	a line of text to be passed
+	 * @return an Arraylist object containing all individual entries found on that line
+	 */
+	public static ArrayList<String> splitCSVLine(String textLine){
+		
+		ArrayList<String> entries = new ArrayList<String>(); 
+		int lineLength = textLine.length(); 
+		StringBuffer nextWord = new StringBuffer(); 
+		char nextChar; 
+		boolean insideQuotes = false; 
+		boolean insideEntry= false;
+		
+		// iterate over all characters in the textLine
+		for (int i = 0; i < lineLength; i++) {
+			nextChar = textLine.charAt(i);
+			
+			// handle smart quotes as well as regular quotes
+			if (nextChar == '"' || nextChar == '\u201C' || nextChar =='\u201D') {
+					
+				// change insideQuotes flag when nextChar is a quote
+				if (insideQuotes) {
+					insideQuotes = false; 
+					insideEntry = false;
+				}else {
+					insideQuotes = true; 
+					insideEntry = true;
+				}
+			} else if (Character.isWhitespace(nextChar)) {
+				if ( insideQuotes || insideEntry ) {
+				// add it to the current entry 
+					nextWord.append( nextChar );
+				}else { // skip all spaces between entries
+					continue; 
+				}
+			} else if ( nextChar == ',') {
+				if (insideQuotes){ // comma inside an entry
+					nextWord.append(nextChar); 
+				} else { // end of entry found
+					insideEntry = false;
+					entries.add(nextWord.toString());
+					nextWord = new StringBuffer();
+				}
+			} else {
+				// add all other characters to the nextWord
+				nextWord.append(nextChar);
+				insideEntry = true;
+			} 
+			
+		}
+		// add the last word ( assuming not empty ) 
+		// trim the white space before adding to the list 
+		if (!nextWord.toString().equals("")) {
+			entries.add(nextWord.toString().trim());
+		}
+
+		return entries;
+	}
+	
